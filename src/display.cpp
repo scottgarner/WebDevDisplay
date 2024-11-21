4f48424f8aec976342b2313f399fee1ca9cfccea
Scott Garner <scott@j38.net>
2024-11-21 13:17:17 -0800

    Reworked display system to support arbitrary graphics buffers transformed through a LUT to the physical display.

--- src/display.cpp
+++ src/display.cpp
@@ -1,187 +1,238 @@
 #include "globals.h"
+#include "config.h"
+#include "lut_8x8.h"
 
-#define REFRESH_INTERVAL 1000
+#define STRIPS 1
+#include <FastLED.h>
 
-////
+int offsetX = 0;
+int offsetY = 0;
 
-uint8_t messageIndex = 0;
-uint lastMessageCycleTime = 0;
+int pins[STRIPS] = {32};
 
-////
+uint8_t graphicsBuffer[16385];
+uint16_t graphicsBufferWidth = 0;
+uint16_t graphicsBufferHeight = 0;
 
-// #define NUM_LEDS_PER_STRIP 256
-// #define STRIPS 12
-#define NUM_LEDS_PER_STRIP 768
-#define STRIPS 4
+uint8_t physicalBuffer[ROWS * COLUMNS * 3];
 
-#include "I2SClocklessLedDriver.h"
+bool refresh = false;
 
-I2SClocklessLedDriver driver;
+long lastScrollTime = 0;
+int scrollDelta = 250;
 
-#define ROWS 8
-#define COLUMNS ((NUM_LEDS_PER_STRIP * STRIPS) / ROWS)
-
-uint8_t leds[STRIPS * NUM_LEDS_PER_STRIP * 3];
-int pins[STRIPS] = {32, 33, 25, 26};
-
-// int pins[STRIPS] = {
-//     32, 33, 25, 26, 27, 14,
-//     19, 18, 05, 17, 16, 04};
-
-////
-
-#include "fonts/HUNTER_font.h"
-#define CHAR_WIDTH 8
-#define CHAR_HEIGHT 8
-
-////
-
-int offset = 0;
-uint lastOffsetTime = 0;
-
-char fontColumns[256 * 8];
-int fontColumnIndex = 0;
-
-uint lastRefreshTime = 0;
-
-////
-
-void displayRefresh()
+int wrap(int value, int divisor)
 {
-
-    char *rawTextBuffer = messages[messageIndex].text_buffer;
-    uint8_t rawTextBufferLength = messages[messageIndex].text_buffer_length;
-
-    // Process variables.
-    String stringBuffer = String(rawTextBuffer, rawTextBufferLength);
-    char time[9];
-    timeNow(time);
-    stringBuffer.replace("{{time}}", time);
-
-    Serial.print("Displaying message: ");
-    Serial.println(stringBuffer);
-
-    Serial.println("Updating pixel buffer...");
-    // Update pixel buffer.
-    {
-        if (stringBuffer.length() <= 0)
-            return;
-
-        fontColumnIndex = 0;
-
-        for (int i = 0; i < stringBuffer.length(); i++)
-        {
-            // Grab letter from buffer;
-            char letter = stringBuffer[i];
-
-            // Change letter into font data index.
-            char index = letter & 0x7F;
-            if (index < ' ')
-            {
-                index = 0;
-            }
-            else
-            {
-                index -= ' ';
-            }
-
-            // Lookup font data.
-            const unsigned char *fontData = &font[index][0];
-
-            // Sample font columns;
-            for (int x = 0; x < CHAR_WIDTH; x++)
-            {
-                fontColumns[fontColumnIndex] = fontData[x];
-                fontColumnIndex++;
-            }
-        }
-    }
+    return ((value % divisor) + divisor) % divisor;
 }
 
-extern void displaySetMessage(int index)
+void graphicsToPhysical()
 {
-    messageIndex = index;
-    displayRefresh();
+    for (int y = 0; y < ROWS; y++)
+    {
+        for (int x = 0; x < COLUMNS; x++)
+        {
+
+            // Sample graphics buffer.
+            int graphicsBufferX = wrap(x + offsetX, graphicsBufferWidth);
+            int graphicsBufferY = wrap(y + offsetY, graphicsBufferHeight);
+            int graphicsIndex = (graphicsBufferY * graphicsBufferWidth + graphicsBufferX) * 3;
+
+            // Get the physical positions from the LUT.
+            int logicalIndex = y * COLUMNS + x;
+            int physicalIndexRed = lut[logicalIndex] * 3;
+            int physicalIndexGreen = lut[logicalIndex] * 3 + 1;
+            int physicalIndexBlue = lut[logicalIndex] * 3 + 2;
+
+            uint8_t red = graphicsBuffer[graphicsIndex];
+            uint8_t green = graphicsBuffer[graphicsIndex + 1];
+            uint8_t blue = graphicsBuffer[graphicsIndex + 2];
+
+            // Copy RGB values to the physical buffer.
+            physicalBuffer[physicalIndexRed] = red;
+            physicalBuffer[physicalIndexGreen] = green;
+            physicalBuffer[physicalIndexBlue] = blue;
+            /*
+                        Serial.print(" COLUMN: ");
+                        Serial.print(x);
+                        Serial.print(" ROW: ");
+                        Serial.print(y);
+
+                        Serial.print(" Logical Index: ");
+                        Serial.print(logicalIndex);
+                        Serial.print(" Graphics Index: ");
+                        Serial.print(graphicsIndex);
+
+                        Serial.print(" Red: ");
+                        Serial.print(red);
+                        Serial.print(" Green: ");
+                        Serial.print(green);
+                        Serial.print(" Blue: ");
+                        Serial.print(blue);
+
+                        Serial.print(" Physical Index Red: ");
+                        Serial.print(physicalIndexRed);
+                        Serial.print(" Physical Index Green: ");
+                        Serial.print(physicalIndexGreen);
+                        Serial.print(" Physical Index Blue: ");
+                        Serial.print(physicalIndexBlue);
+
+                        Serial.println();
+            */
+        }
+    }
+    /*
+        for (int i = 0; i < ROWS * COLUMNS * 3; i++)
+        {
+            Serial.print(physicalBuffer[i]);
+            Serial.print(',');
+        }
+        Serial.println();
+    */
+}
+
+void loadBuffer()
+{
+    if (!LittleFS.begin(true))
+    {
+        Serial.println("Failed to mount LittleFS!");
+        return;
+    }
+
+    PixelDataHeader *header;
+    File file = LittleFS.open("/buffer.dat", FILE_READ);
+
+    if (!file)
+    {
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    size_t fileSize = file.size();           // Get the total size of the file
+    uint8_t *buffer = new uint8_t[fileSize]; // Dynamically allocate memory for the file contents
+
+    if (!buffer)
+    {
+        Serial.println("Failed to allocate memory for file data");
+        file.close();
+        return;
+    }
+
+    file.read(buffer, fileSize); // Read the entire file into the buffer
+    file.close();
+
+    displaySetBuffer(buffer, fileSize, false);
+
+    Serial.println("Buffer successfully loaded from LittleFS");
+    delete[] buffer;
+}
+
+void saveBuffer(uint8_t *buffer, size_t bufferSize)
+{
+    Serial.println("Save buffer!");
+
+    if (!LittleFS.begin(true))
+    {
+        Serial.println("LittleFS Mount Failed");
+        return;
+    }
+
+    File file = LittleFS.open("/buffer.dat", FILE_WRITE);
+    if (!file)
+    {
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    file.write(buffer, bufferSize); // Write the buffer to the file
+    file.close();
+    Serial.println("Buffer saved successfully");
 }
 
 void displaySetup()
 {
-    driver.initled(leds, pins, STRIPS, NUM_LEDS_PER_STRIP, ORDER_GRB);
+    Serial.println("Display setup...");
 
-    for (int i = 0; i < NUM_LEDS_PER_STRIP * STRIPS; i++)
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>((CRGB *)physicalBuffer, NUM_LEDS_PER_STRIP);
+    FastLED.setBrightness(BRIGHTNESS);
+
+    lastScrollTime = millis();
+
+    graphicsBuffer[0] = 0;
+    graphicsBuffer[1] = 0;
+    graphicsBuffer[2] = 0;
+    graphicsBuffer[3] = 0xFF;
+    graphicsBuffer[4] = 0;
+    graphicsBuffer[5] = 0;
+    graphicsBuffer[6] = 0xFF;
+    graphicsBuffer[7] = 0;
+    graphicsBuffer[8] = 0;
+    graphicsBuffer[9] = 0;
+    graphicsBuffer[10] = 0;
+    graphicsBuffer[11] = 0;
+
+    graphicsBufferWidth = 2;
+    graphicsBufferHeight = 2;
+
+    loadBuffer();
+
+    refresh = true;
+
+    Serial.println("Display setup complete...");
+}
+
+void displaySetBuffer(uint8_t *buffer, size_t bufferSize, bool save = false)
+{
+    saveBuffer(buffer, bufferSize);
+
+    PixelDataHeader *header = (PixelDataHeader *)buffer;
+    uint8_t *pixelData = buffer + sizeof(PixelDataHeader);
+    /*
+        Serial.print("Width: ");
+        Serial.println(header->width);
+        Serial.print("Height: ");
+        Serial.println(header->height);
+        Serial.print("Depth: ");
+        Serial.println(header->bitDepth);
+        Serial.println("Display set graphics buffer...");
+    */
+
+    // Ensure the buffer is not larger than the memory can handle
+    uint32_t pixelBufferSize = header->width * header->height * 3;
+    if (pixelBufferSize > sizeof(graphicsBuffer))
     {
-        float a = (i % 2 == 0) ? 128 : 0;
-        driver.setPixel(i, a, a, a);
+        Serial.println("Error: Buffer size exceeds available memory.");
+        return;
     }
 
-    driver.setBrightness(configuration.brightness);
-    driver.showPixels();
+    memcpy(graphicsBuffer, pixelData, bufferSize);
+    graphicsBufferWidth = header->width;
+    graphicsBufferHeight = header->height;
 
-    displayRefresh();
+    refresh = true;
 }
 
 void displayLoop()
 {
-    bool redraw = false;
-
-    // Handle scrolling.
-    if (messages[messageIndex].speed > 0)
+    if (refresh)
     {
-        if (millis() - lastOffsetTime > (1000.0 / messages[messageIndex].speed))
+        graphicsToPhysical();
+        // driver.showPixels();
+        FastLED.show();
+        // Serial.println("Refresh!");
+        refresh = false;
+    }
+
+    if (millis() - lastScrollTime > scrollDelta)
+    {
+        lastScrollTime = millis();
+
+        if (graphicsBufferWidth > COLUMNS)
         {
-            offset++;
-            lastOffsetTime = millis();
-            redraw = true;
-        }
-    }
-
-    // Handle swapping.
-    if (messageCount > 0 && millis() - lastMessageCycleTime > (messages[messageIndex].duration * 1000.0))
-    {
-        messageIndex = (messageIndex + 1) % messageCount;
-        // offset = 0;
-        displayRefresh();
-        lastMessageCycleTime = millis();
-        redraw = true;
-    }
-
-    // Handle refresh interval.
-    if (millis() - lastRefreshTime > REFRESH_INTERVAL)
-    {
-        displayRefresh();
-        lastRefreshTime = millis();
-        redraw = true;
-    }
-
-    // Set pixels.
-    if (redraw)
-    {
-
-        for (int x = 0; x < COLUMNS; x++)
-        {
-            int column = (x + offset) % fontColumnIndex;
-            char fontColumn = fontColumns[column];
-
-            for (int y = 0; y < CHAR_HEIGHT; y++)
-            {
-                int pixelIndex = (x * ROWS) + ((x % 2 == 0) ? y : 7 - y);
-
-                if (fontColumn & (1 << y))
-                {
-                    uint red = messages[messageIndex].red;
-                    uint green = messages[messageIndex].green;
-                    uint blue = messages[messageIndex].blue;
-
-                    driver.setPixel(pixelIndex, red, green, blue);
-                }
-                else
-                {
-                    driver.setPixel(pixelIndex, 0, 0, 0);
-                }
-            }
+            offsetX = (offsetX + 1) % graphicsBufferWidth;
+            refresh = true;
         }
 
-        // Show pixels.
-        driver.showPixels();
+        // offsetY = (offsetY + 1) % ROWS;
     }
 }
