baead02e4924fb22635cd4fc7452cec1690db6be
Scott Garner <scott@j38.net>
2024-11-21 13:14:23 -0800

    Main cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,11 @@
 #include "globals.h"
 
-// #define configCHECK_FOR_STACK_OVERFLOW 2
-
-// void networkingTask(void *parameter)
-// {
-//   while (1)
-//   {
-//     networkingLoop();
-//     taskYIELD();
-//   }
-// }
-
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("READY");
 
-  dataLoad();
   displaySetup();
-  networkingSetup([]()
-                  { timeSetup(); });
-
-  // xTaskCreatePinnedToCore(
-  //     networkingTask,   /* Task function. */
-  //     "networkingTask", /* String with name of task. */
-  //     1024 * 8,         /* Stack size in bytes. */
-  //     NULL,             /* Parameter passed as input of the task */
-  //     4,                /* Priority of the task. */
-  //     NULL,             /* Task handle. */
-  //     0);               /* CPU core. */
+  networkingSetup();
 }
 
 void loop()
