77c244070440bfbddaf7f7fd41299817a425e1f1
Scott Garner <scott@j38.net>
2024-11-21 13:16:14 -0800

    Reworked network handling for WS/POST graphics bufffers. Connection handling with WiFiManager.

--- src/networking.cpp
+++ src/networking.cpp
@@ -1,117 +1,181 @@
 #include "globals.h"
 
 #include <ArduinoOTA.h>
-#include <ESPmDNS.h>
-#include <SPIFFS.h>
-#include <WebServer.h>
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiUdp.h>
+#include <ArduinoJson.h>
 
+#include <WiFiManager.h>
 #include <ESPAsyncWebServer.h>
 
-#define HOSTNAME "scoreboard"
-#define OTA_PORT 3232
+WiFiManager wm;
 
-AsyncWebServer server(80);
+AsyncWebServer server(HTTP_PORT);
+AsyncWebSocket ws("/ws");
 
-bool initialized = false;
-void (*onInitalized)();
+uint8_t buffer[16385];
+int bufferSize = 0;
 
-void networkingSetup(void (*callback)())
+void processBuffer(uint8_t *buffer, size_t bufferSize)
 {
-    Serial.println("Configuring access point...");
+    displaySetBuffer(buffer, bufferSize, true);
+}
 
-    Serial.print("Core: ");
-    Serial.println(CONFIG_ASYNC_TCP_RUNNING_CORE);
+void networkingSetup()
+{
+    // wm.resetSettings();
+    wm.setConfigPortalBlocking(false);
+    wm.setWiFiAutoReconnect(true);
+    wm.setHostname(HOSTNAME);
 
-    // WIFI.
-    // WiFi.mode(WIFI_MODE_APSTA);
-    WiFi.mode(WIFI_MODE_STA);
-
-    // AP Setup.
-    /*
+    if (wm.autoConnect(HOSTNAME))
     {
-        WiFi.softAP(configuration.ap_ssid, configuration.ap_password);
-
-        Serial.print("Connecting to: ");
-        Serial.println(configuration.ap_ssid);
-
-        Serial.print("ESP32 IP as soft AP: ");
-        Serial.println(WiFi.softAPIP());
-    }
-    */
-
-    if (!SPIFFS.begin(true))
-    {
-        Serial.println("An Error has occurred while mounting SPIFFS");
-        return;
-    }
-
-    server.on("/data", HTTP_GET,
-              [](AsyncWebServerRequest *request)
-              {
-                  AsyncResponseStream *response = request->beginResponseStream("application/json");
-                  serializeJson(dataGetJson(), *response);
-                  request->send(response);
-              });
-
-    server.on(
-        "/data", HTTP_POST,
-        [](AsyncWebServerRequest *request) {},
-        NULL,
-        [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
-        {
-            Serial.println("Handling post request...");
-            dataParseJson(data);
-            Serial.println("Sending response...");
-            request->send(200, "text/plain", "OK");
-        });
-
-    server.serveStatic("/assets/index.js", SPIFFS, "/assets/index.js").setCacheControl("max-age=31536000");
-    server.serveStatic("/assets/index.css", SPIFFS, "/assets/index.css").setCacheControl("max-age=31536000");
-    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html").setCacheControl("max-age=31536000");
-
-    server.onNotFound([](AsyncWebServerRequest *request)
-                      { if (request->method() == HTTP_OPTIONS) {
-                            request->send(200);
-                        } else {
-                            request->send(404);
-                        } });
-
-    DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
-    server.begin();
-
-    Serial.println("Server started");
-
-    if (!MDNS.begin(HOSTNAME))
-    { // Start the mDNS responder for esp8266.local
-        Serial.println("Error setting up MDNS responder!");
+        Serial.println("Connected!");
     }
     else
     {
-        Serial.println("MDNS enabled.");
-        MDNS.enableArduino(OTA_PORT, false);
-        MDNS.addService("http", "tcp", 80);
+        Serial.println("No connection. Running config porta.");
     }
 
+    // Default headers.
+    {
+        DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
+        DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+    }
+
+    // Server routes.
+    {
+        server.on(
+            "/status", HTTP_GET,
+            [](AsyncWebServerRequest *request)
+            {
+                StaticJsonDocument<200> jsonDoc;
+
+                jsonDoc["SSID"] = WiFi.SSID();
+                jsonDoc["IP"] = WiFi.localIP().toString();
+                jsonDoc["RSSI"] = WiFi.RSSI();
+
+                String jsonString;
+                serializeJson(jsonDoc, jsonString);
+
+                request->send(200, "application/json", jsonString);
+            });
+
+        server.on(
+            "/buffer", HTTP_GET,
+            [](AsyncWebServerRequest *request)
+            {
+                request->send(200, "text/plain", "OK");
+            });
+
+        server.on(
+            "/buffer", HTTP_POST,
+            [](AsyncWebServerRequest *request) {},
+            NULL,
+            [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
+            {
+                if (index == 0)
+                {
+                    Serial.printf("BodyStart: %u\n", total);
+                    bufferSize = 0;
+                }
+
+                Serial.printf("Length: %u/%u \n", len, total);
+                for (size_t i = 0; i < len; i++)
+                {
+                    buffer[bufferSize] = data[i];
+                    bufferSize++;
+                }
+
+                if (index + len == total)
+                {
+                    Serial.printf("BodyEnd: %u %u\n", total, bufferSize);
+                    processBuffer(buffer, bufferSize);
+                    request->send(200, "text/plain", "OK");
+                }
+            });
+
+        server.onNotFound([](AsyncWebServerRequest *request)
+                          { 
+                        if (request->method() == HTTP_OPTIONS) {
+                            Serial.println("Options!");
+                            request->send(200, "text/plain", "OK");
+                        } else {
+                            Serial.println("404?");
+                            request->send(200, "text/plain", "OK?");
+                        } });
+    }
+
+    // Websocket handler.
+    {
+        ws.onEvent(
+            [](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+            {
+                if (type == WS_EVT_CONNECT)
+                {
+                    Serial.println("Client connected");
+                    client->text("Hello Client!");
+                }
+                else if (type == WS_EVT_DISCONNECT)
+                {
+                    Serial.println("Client disconnected");
+                }
+                else if (type == WS_EVT_DATA)
+                {
+                    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+
+                    if (info->index == 0)
+                    {
+                        bufferSize = 0;
+                    }
+
+                    for (size_t i = 0; i < len; i++)
+                    {
+                        buffer[bufferSize] = data[i];
+                        bufferSize++;
+                    }
+
+                    if (info->final)
+                    {
+                        // Serial.println("Last frame received");
+                        // Serial.printf("Received full data: %u bytes\n", bufferSize);
+                        processBuffer(buffer, bufferSize);
+                    }
+                }
+            });
+        server.addHandler(&ws);
+    }
+
+    // Static files.
+    {
+
+        if (!LittleFS.begin(true))
+        {
+            Serial.println("An Error has occurred while mounting LittleFS!");
+        }
+
+        server.serveStatic("/assets/index.js", LittleFS, "/assets/index.js").setCacheControl("max-age=31536000");
+        server.serveStatic("/assets/index.css", LittleFS, "/assets/index.css").setCacheControl("max-age=31536000");
+        server.serveStatic("/", LittleFS, "/").setDefaultFile("index.html").setCacheControl("max-age=31536000");
+    }
+
+    // Server begin.
+    server.begin();
+
     // OTA updates.
+    {
+        ArduinoOTA.setHostname(HOSTNAME);
+        ArduinoOTA.setPort(OTA_PORT);
+        ArduinoOTA.setMdnsEnabled(true);
 
-    ArduinoOTA.setHostname(HOSTNAME);
-    ArduinoOTA.setPort(OTA_PORT);
-    ArduinoOTA.setMdnsEnabled(false);
-
-    //   ArduinoOTA.setPassword("admin");
-
-    ArduinoOTA
-        .onStart([]()
-                 { Serial.println("Starting update..."); })
-        .onEnd([]()
-               { Serial.println("\nEnd"); })
-        .onProgress([](unsigned int progress, unsigned int total)
-                    { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-        .onError([](ota_error_t error)
-                 {
+        ArduinoOTA
+            .onStart([]()
+                     { Serial.println("Starting update..."); })
+            .onEnd([]()
+                   { Serial.println("\nEnd"); })
+            .onProgress([](unsigned int progress, unsigned int total)
+                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+            .onError([](ota_error_t error)
+                     {
                     Serial.printf("Error[%u]: ", error);
                     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
                     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
@@ -119,35 +183,13 @@ void networkingSetup(void (*callback)())
                     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
                     else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
 
-    ArduinoOTA.begin();
-
-    // Station setup.
-    {
-        Serial.print("Connecting to SSID: ");
-        Serial.println(configuration.wifi_ssid);
-
-        WiFi.begin(configuration.wifi_ssid, configuration.wifi_password);
-        WiFi.setAutoReconnect(true);
-        WiFi.setSleep(false);
+        ArduinoOTA.begin();
     }
-
-    onInitalized = callback;
 }
 
 void networkingLoop()
 {
-    if (!initialized && WiFi.status() == WL_CONNECTED)
-    {
-        Serial.print("ESP32 IP on the WiFi network: ");
-        Serial.println(WiFi.localIP());
-
-        onInitalized();
-        initialized = true;
-    }
-    else if (!initialized)
-    {
-        // Serial.print(WiFi.status());
-    }
-
+    wm.process();
+    ws.cleanupClients();
     ArduinoOTA.handle();
 }
