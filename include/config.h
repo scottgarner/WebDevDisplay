4f8b01c9f2145e484c30d53b8c21c6cfe24bdca8
Scott Garner <scott@j38.net>
2024-11-21 13:10:40 -0800

    Updated config and sample LUTs.

---
+++ include/config.h
@@ -0,0 +1,13 @@
+#define HOSTNAME "scoreboard"
+#define BRIGHTNESS 4
+
+#define HTTP_PORT 80
+#define OTA_PORT 3232
+
+#define COLUMNS 8
+#define ROWS 8
+
+#define NUM_LEDS_PER_STRIP 64
+#define DATA_PIN D10
+
+#define GRAPHICS_BUFFER_MAX 1 << 15
